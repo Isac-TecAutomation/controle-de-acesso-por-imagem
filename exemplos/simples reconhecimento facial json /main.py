35aae381875c9f53fd63d1fc7d420b1746c62ca1
Isac-TecAutomation <isaceugenio564@gmail.com>
2025-01-08 11:41:12 -0300

    - atualização nesse exemplo

--- exemplos/simples reconhecimento facial json /main.py
+++ exemplos/simples reconhecimento facial json /main.py
@@ -15,16 +15,9 @@ from comandos import Commands
 
 # Inicializa o objeto de controle de acesso com as configurações específicas.
 control = Commands(
-    host_webcam= 'http://192.168.0.20/800x600.jpg',     #'<host da sua webcam>',  # Endereço da webcam para capturar imagem de reconhecimento facial.
-    host_database='<host do seu database>',  # Endereço do servidor MySQL (localhost).
-    user_database='<usuario do seu banco de dados>',  # Usuário do banco de dados (troque por seu usuário).
-    password='<sua senha usuario do DB>',  # Senha do banco de dados (troque por sua senha).
-    database_name='db_controle_de_acesso_por_imagem',  # Nome do banco de dados.
-    image_column='<coluna de encoding no DB>',  # Nome da coluna onde as imagens dos usuários estão armazenadas no banco.
-    other_columns= 'tupla com as outras colunas do tabela',  # Outras colunas que são relevantes para o controle de acesso.
-    user_table='<nome da tabela pertecente a usuarios>',  # Nome da tabela de usuários no banco de dados.
-    device_table='<tabela de dispositivos permitidos para o acesso>',  # Nome da tabela de dispositivos no banco de dados.
-    mac_column='<coluna com mac para esse dispositivos>'  # Coluna que armazena os endereços MAC dos dispositivos.
+    host_webcam='<host_da_sua_webcam>',  # Exemplo: 'http://<endereço_ip>/800x600.jpg'
+    dir_archive_json='<caminho_do_arquivo_json>',  # Exemplo: 'caminho/para/arquivo.json'
+    image_column='<nome_da_coluna_de_imagem>'  # Exemplo: 'encoding'
 )
 
 # Parte principal do código
@@ -36,15 +29,10 @@ if __name__ == '__main__':
     # IMPORTANTE: comente a parte que você não irá testar para evitaar conflitos
 
     # VERIFICAR ROSTO:
-    # A função face_verify_json recebe o limite de confiança, o caminho do arquivo JSON com os dados 
-    # e o nome da chave (encoding). Este método será usado para verificar se um rosto capturado 
-    # corresponde a algum rosto registrado no banco de dados.
-    #
-    # O primeiro parâmetro define o limite de confiança. Quanto mais alto o número, mais rigorosa será 
+    # A função face_verify_json contém o parâmetro define o limite de confiança. Quanto mais alto o número, mais rigorosa será 
     # a verificação.
 
-    Verificar = control.face_verify_json('<nivel de confiança minimo (int)>', 
-                                        '<diretório>dados.json', '<chave correspondente aos encodings>')
+    Verificar = control.face_verify_json(60)
     
     # Exibe o resultado da verificação de rosto. Se a correspondência for bem-sucedida, serão mostrados os dados
     # do usuário correspondente.
@@ -61,7 +49,7 @@ if __name__ == '__main__':
     dados_novo_usuario = {}
      
     # essa é disposição correta:
-    Registrar = control.register_face_json('<diretório>dados.json', '<chave correspondente aos encodings>', dados_novo_usuario)
+    Registrar = control.register_face_json(dados_novo_usuario)
     
     # A função register_face_json adiciona um novo rosto ao banco de dados. O arquivo JSON será atualizado com
     # o novo rosto e os dados relacionados.
